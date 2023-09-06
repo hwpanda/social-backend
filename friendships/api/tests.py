b12bbb2e169bb064f6baf35c1c698c625080ac4f
Haobo <48510618+hwpanda@users.noreply.github.com>
2023-09-05 20:44:21 -0700

    friendship unit test

---
+++ friendships/api/tests.py
@@ -0,0 +1,138 @@
+from testing.testcases import TestCase
+from rest_framework.test import APIClient
+from friendships.models import Friendship
+
+FRIENDSHIP_FOLLOWER_URL = '/api/friendships/{}/followers/'
+FRIENDSHIP_FOLLOWING_URL = '/api/friendships/{}/followings/'
+FRIENDSHIP_FOLLOW_URL = '/api/friendships/{}/follow/'
+FRIENDSHIP_UNFOLLOW_URL = '/api/friendships/{}/unfollow/'
+
+
+class FriendshipApiTests(TestCase):
+
+    def setUp(self):
+        self.clear_cache()
+        self.user1 = self.create_user("user1", "user1@testing.com")
+        self.user1_client = APIClient()
+        self.user1_client.force_authenticate(self.user1)
+
+        self.user2 = self.create_user("user2", "user2@testing.com")
+        self.user2_client = APIClient()
+        self.user2_client.force_authenticate(self.user2)
+
+        for i in range(2):
+            follower = self.create_user('user1_follower{}'.format(i))
+            Friendship.objects.create(from_user=follower, to_user=self.user1)
+        for i in range(3):
+            following = self.create_user('user1_following{}'.format(i))
+            Friendship.objects.create(from_user=self.user1, to_user=following)
+
+    def test_follow(self):
+        url = FRIENDSHIP_FOLLOW_URL.format(self.user1.id)
+
+        # need to login
+        response = self.anonymous_client.post(url)
+        self.assertEqual(response.status_code, 403)
+
+        # need to use GET to follow
+        response = self.user1_client.get(url)
+        self.assertEqual(response.status_code, 405)
+
+        # cannot follow yourself
+        response = self.user1_client.post(url)
+        self.assertEqual(response.status_code, 400)
+
+        # follow success
+        response = self.user2_client.post(url)
+        self.assertEqual(response.status_code, 201)
+
+        # prevent duplicate
+        response = self.user2_client.post(url)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['duplicate'], True)
+
+        # follow back will create new data
+        count = Friendship.objects.count()
+        response = self.user1_client.post(
+            FRIENDSHIP_FOLLOW_URL.format(self.user2.id))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Friendship.objects.count(), count + 1)
+
+    def test_unfollow(self):
+        url = FRIENDSHIP_UNFOLLOW_URL.format(self.user1.id)
+
+        # need to login to unfollow
+        response = self.anonymous_client.post(url)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.user1_client.get(url)
+        self.assertEqual(response.status_code, 405)
+
+        # cannot unfollow yourself
+        response = self.user1_client.post(url)
+        self.assertEqual(response.status_code, 400)
+
+        # unfollow success
+        Friendship.objects.create(from_user=self.user2, to_user=self.user1)
+        count = Friendship.objects.count()
+        response = self.user2_client.post(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['deleted'], 1)
+        self.assertEqual(Friendship.objects.count(), count-1)
+
+        count = Friendship.objects.count()
+        response = self.user2_client.post(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['deleted'], 0)
+        self.assertEqual(Friendship.objects.count(), count)
+
+    def test_followings(self):
+        url = FRIENDSHIP_FOLLOWING_URL.format(self.user1.id)
+
+        response = self.anonymous_client.post(url)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.user1_client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 3)
+
+        ts0 = response.data['results'][0]['created_at']
+        ts1 = response.data['results'][1]['created_at']
+        ts2 = response.data['results'][2]['created_at']
+        self.assertEqual(ts0 > ts1, True)
+        self.assertEqual(ts1 > ts2, True)
+        self.assertEqual(
+            response.data['results'][0]['user']['username'],
+            'user1_following2'
+        )
+        self.assertEqual(
+            response.data['results'][1]['user']['username'],
+            'user1_following1'
+        )
+        self.assertEqual(
+            response.data['results'][2]['user']['username'],
+            'user1_following0'
+        )
+
+    def test_followers(self):
+
+        url = FRIENDSHIP_FOLLOWER_URL.format(self.user1.id)
+
+        response = self.anonymous_client.post(url)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.anonymous_client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 2)
+
+        ts0 = response.data['results'][0]['created_at']
+        ts1 = response.data['results'][1]['created_at']
+        self.assertEqual(ts0 > ts1, True)
+        self.assertEqual(
+            response.data['results'][0]['user']['username'],
+            'user1_follower1'
+        )
+        self.assertEqual(
+            response.data['results'][1]['user']['username'],
+            'user1_follower0'
+        )
--- friendships/api/views.py
+++ friendships/api/views.py
@@ -15,6 +15,7 @@ class FriendshipViewSet(viewsets.GenericViewSet):
     # POST /api/friendship/1/follow is to follow user_id=1
     # so we query User, instead of Friendship
     queryset = User.objects.all()
+    serializer_class = FriendshipSerializerForCreate
 
     def list(self, request):
         # GET /api/friendships/
@@ -46,6 +47,8 @@ class FriendshipViewSet(viewsets.GenericViewSet):
     def follow(self, request, pk):
         # check repeat follow, like click multiple times
         # ignore
+
+        # self.get_object()
         if Friendship.objects.filter(from_user=request.user, to_user=pk).exists():
             return Response({
                 'success': True,
@@ -69,10 +72,17 @@ class FriendshipViewSet(viewsets.GenericViewSet):
             status=status.HTTP_201_CREATED
         )
 
+    # https://docs.djangoproject.com/en/4.2/ref/models/querysets/#delete
+    # Performs an SQL delete query on all rows in the QuerySet and returns the number of objects deleted and a dictionary with the number of deletions per object type.
+    # The delete() is applied instantly. You cannot call delete() on a QuerySet that has had a slice taken or can otherwise no longer be filtered.
+    # By default, Django’s ForeignKey emulates the SQL constraint ON DELETE CASCADE — in other words, any objects with foreign keys pointing at the objects to be deleted will be deleted along with them; domino effect
+
+    # ! CASCADE is dangerous, so use on_delete=models.SET_NULL
+
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def unfollow(self, request, pk):
-        unfollowuser = self.get_object()
-        if request.user.id == int(unfollowuser.id):
+        unfollow_user = self.get_object()
+        if request.user.id == int(unfollow_user.id):
             return Response({
                 'success': False,
                 'message': "You cannot unfollow yourself",
@@ -80,7 +90,7 @@ class FriendshipViewSet(viewsets.GenericViewSet):
 
         deleted, _ = Friendship.objects.filter(
             from_user=request.user,
-            to_user=unfollowuser,
+            to_user=unfollow_user,
         ).delete()
 
         return Response({
